89af9f69bf49d164615369b7da040776de8e5f92
Dimulski <george.dimulski@gmail.com>
2016-01-18 19:55:27 +0200

    Added Defensive Programming and Exceptions Homework

---
+++ High-Quality Code/09. Defensive Programming and Exceptions/Homework/Assertions-and-Exceptions/Exceptions/ExamResult.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class ExamResult
+{
+    public ExamResult(int grade, int minGrade, int maxGrade, string comments)
+    {
+        if (grade < 0)
+        {
+            throw new ArgumentException("Grade cannot be negative!", "grade");
+        }
+
+        if (minGrade < 0)
+        {
+            throw new ArgumentException("MinGrade cannot be negative!", "minGrade");
+        }
+
+        if (maxGrade <= minGrade)
+        {
+            throw new ArgumentException("MaxGrade must be greater than Min Grade!", "maxGrade");
+        }
+
+        if (string.IsNullOrEmpty(comments))
+        {
+            throw new ArgumentNullException("Comments can no be null or empty!","comments");
+        }
+
+        this.Grade = grade;
+        this.MinGrade = minGrade;
+        this.MaxGrade = maxGrade;
+        this.Comments = comments;
+    }
+
+    public int Grade { get; private set; }
+
+    public int MinGrade { get; private set; }
+
+    public int MaxGrade { get; private set; }
+
+    public string Comments { get; private set; }
+}
