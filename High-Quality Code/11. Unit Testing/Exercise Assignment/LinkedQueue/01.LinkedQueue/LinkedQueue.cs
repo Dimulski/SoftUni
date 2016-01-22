3cf839a9da8704fa9799422d8175291edefbe8b5
Dimulski <george.dimulski@gmail.com>
2016-01-22 21:12:50 +0200

    Added Unit Testing Exercise

---
+++ High-Quality Code/11. Unit Testing/Exercise Assignment/LinkedQueue/01.LinkedQueue/LinkedQueue.cs
@@ -0,0 +1,49 @@
+﻿namespace _01.LinkedQueue
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class LinkedQueue<T> : IEnumerable<T>
+    {
+        private QueueNode<T> head;
+        private QueueNode<T> tail;
+
+        public int Count { get; private set; }
+
+        public void Enqueue(T element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Dequeue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Peek()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ High-Quality Code/11. Unit Testing/Exercise/LinkedQueue/LinkedQueue.Tests/LinkedQueueTests.cs
@@ -0,0 +1,86 @@
+﻿namespace LinkedQueue.Tests
+{
+    using System;
+    using System.Text;
+    using _01.LinkedQueue;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class LinkedQueueTests
+    {
+        private LinkedQueue<int> queue;
+
+        [TestInitialize]
+        public void QueueInitialize()
+        {
+            this.queue = new LinkedQueue<int>();
+        }
+
+        [TestMethod]
+        public void LinkedQueue_EnqueueDequeue_ShouldReturnExpectedElement()
+        {
+            // Arrange
+            var element = 5;
+
+            // Act
+            this.queue.Enqueue(element);
+            this.queue.Dequeue();
+
+            // Assert
+            Assert.AreEqual(0, this.queue.Count);
+        }
+
+        [TestMethod]
+        public void Dequeue_EnqueueDequeueSeveralElements_ShouldReturnElementsInCorrectOrder()
+        {
+            var firstElement = 5;
+            var secondElement = 10;
+            var thirdElement = 20;
+            var stringBuilder = new StringBuilder();
+
+            this.queue.Enqueue(firstElement);
+            this.queue.Enqueue(secondElement);
+            this.queue.Enqueue(thirdElement);
+            stringBuilder.Append(this.queue.Dequeue() + " ");
+            stringBuilder.Append(this.queue.Dequeue() + " ");
+            stringBuilder.Append(this.queue.Dequeue().ToString());
+
+            Assert.AreEqual("5 10 20", stringBuilder.ToString(), "The string should match the stringbuilder value!");
+        }
+
+        [TestMethod]
+        public void Count_EnqueueElements_ShouldReturnCorrectCount()
+        {
+            this.queue.Enqueue(94);
+            Assert.AreEqual(1, this.queue.Count, "The LinkedQueue.Count should show the correct count of the queue!");
+
+            this.queue.Enqueue(80);
+            Assert.AreEqual(2, this.queue.Count, "The LinkedQueue.Count should show the correct count of the queue!");
+
+            this.queue.Enqueue(61);
+            Assert.AreEqual(3, this.queue.Count, "The LinkedQueue.Count should show the correct count of the queue!");
+        }
+
+        [TestMethod]
+        public void Count_EnqueueDequeueElements_ShouldReturnCorrectCount()
+        {
+            this.queue.Enqueue(88);
+            this.queue.Enqueue(41);
+            this.queue.Enqueue(54);
+            this.queue.Enqueue(50);
+            this.queue.Dequeue();
+            Assert.AreEqual(3, this.queue.Count);
+
+            this.queue.Dequeue();
+            this.queue.Dequeue();
+            Assert.AreEqual(1, this.queue.Count);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Dequeue_EmptyQueue_ShouldThrow()
+        {
+            this.queue.Dequeue();
+        }
+    }
+}
