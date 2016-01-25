560de690bed61063fe14b6f1218bb5f6bc17727e
Dimulski <george.dimulski@gmail.com>
2016-01-25 22:02:38 +0200

    Added 19. Lab

---
+++ High-Quality Code/19. Lab/Lab Assignment/Huy-Phuong/Huy-Phuong/Class1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+
+
+using System.Collections.Generic;
+using System.Linq;
+
+
+using System.Text;
+
+
+
+using System.Threading.Tasks;
+
+namespace Huy_Phuong
+{
+                static class Class1
+                {
+                    internal static string ExecuteAddTheatreCommand(string[] parameters)
+                    {
+                        string theatreName = parameters[0];
+    NhaHat.universal.AddTheatre(theatreName);
+    return "Theatre added";
+}
+
+                        public static string ExecutePrintAllTheatresCommand()
+                        {
+     var theatresCount = NhaHat
+          .
+           
+          universal
+              .
+              
+              
+                 ListTheatres
+                    (
+                    
+                     )
+                     
+                     
+                     . 
+                       Count
+                          
+                       (
+                          
+                           )   
+                               ;
+                                if (theatresCount > 0) {
+                                    var resultTheatres = new LinkedList<string>();
+                     for (int i = 0; i < theatresCount; i++)
+                {
+                    NhaHat.universal.ListTheatres()
+                        
+                        .Skip(i).ToList()
+                        
+                        
+            .ForEach(
+            t => resultTheatres.
+                            
+                            
+                AddLast(t));
+                    foreach (var t in NhaHat.
+                        
+                        universal.ListTheatres().Skip(
+                        
+                        
+                        
+                        
+                        i + 1)){resultTheatres.Remove(
+                            
+                            
+                            
+                            t);}}return String.Join(", ", resultTheatres);
+            }return "No theatres";
+        }
+    }
+}
---
+++ High-Quality Code/19. Lab/Lab Assignment/Huy-Phuong/Huy-Phuong/Class2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using
+
+
+
+            System.Collections.Generic;
+
+
+       using System.Linq;
+            using System.Text;
+using System.Threading.Tasks;
+
+                namespace Huy_Phuong
+                {
+                                        internal partial class NhaHat
+                                    {
+                                    protected internal static string ExecutePrintAllPerformancesCommand()
+                                    {
+                                        var performances = NhaHat.universal.ListAllPerformances().ToList();
+                    var result = String.Empty;
+                    if (performances.Any())
+    {
+        for (int i = 0; i < performances.Count; i++)
+        {
+            var sb = new StringBuilder();
+                    sb.Append(result);
+                    if (i > 0)
+                    {
+                        sb.Append(", ");
+                    }
+                    string result1 = performances[i].s2.ToString("dd.MM.yyyy HH:mm");
+                    sb.AppendFormat(
+        "({0}, {1}, {2})", performances
+        
+        
+        
+        
+        [i].tr32, performances                                                                                                                                                                                                                                  [i].tr23, result1);
+    result = sb + "";
+                }
+                return result;
+            }
+
+return "No performances";
+}
+    }
+}
