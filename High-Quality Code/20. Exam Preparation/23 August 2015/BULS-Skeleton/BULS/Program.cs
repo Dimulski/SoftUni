e0a83f15fe1b2e7ca54da17de1de541e639548b2
Dimulski <george.dimulski@gmail.com>
2016-02-01 21:36:40 +0200

    Added 20. Exam Preparation

---
+++ High-Quality Code/20. Exam Preparation/23 August 2015/BULS-Skeleton/BULS/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace buls
+{
+    using Core;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var इंजन = new बंगलौरविश्वविद्यालयEngine();
+            इंजन.Run();
+        }
+    }
+}
