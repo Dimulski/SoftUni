2a5b527955be7269171e18a3c5ff686a59a2a950
Dimulski <george.dimulski@gmail.com>
2016-02-19 19:10:28 +0200

    Added Data Structures, Algorithms and Complexity Exercise and Homework

---
+++ Data Structures/01. Data Structures, Algorithms and Complexity/Exercise/Exercise/Problem7CompareExecutionSpeed/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Problem7CompareExecutionSpeed
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            var startTime = DateTime.Now;
+            for (int i = 0; i < 1000000; i++)
+            {
+                IsPrimeFast(i);
+            }
+            var executionTime =
+                DateTime.Now - startTime;
+            Console.WriteLine("Execution time: {0}",
+                executionTime);
+        }
+
+        static bool IsPrime(long num)
+        {
+            for (int i = 2; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static bool IsPrimeFast(long num)
+        {
+            int maxDivisor = (int)Math.Sqrt(num);
+            for (int i = 2; i <= maxDivisor; i++)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
