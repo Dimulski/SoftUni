5de0249ca3004d0648be7705fa1154fcf745023a
Dimulski <george.dimulski@gmail.com>
2016-02-26 23:21:54 +0200

    Added Exam 19 July 2015

---
+++ Advanced C#/09. Exam Preparation/Exam 19 July 2015/Exam 19 July 2015/Test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int current = 0;
+
+            int offset = -1;
+            int arrayLength = 5;
+            int wanted = (current + offset) % arrayLength;
+            Console.WriteLine(wanted);
+        }
+    }
+}
