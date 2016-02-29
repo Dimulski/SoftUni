71e79d57fecf067dd724ebcf08f7df5d6aeccdfb
Dimulski <george.dimulski@gmail.com>
2016-02-29 21:39:11 +0200

    Added Trees and Tree-Like Structures Exercise

---
+++ Data Structures/04. Trees and Tree-Like Structures/Exercise/4. Trees/Binary-Tree/BinaryTree.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class BinaryTree<T>
+{
+    public BinaryTree(T value, BinaryTree<T> leftChild = null, BinaryTree<T> rightChild = null)
+    {
+        // TODO
+    }
+
+    public void PrintIndentedPreOrder(int indent = 0)
+    {
+        // TODO
+    }
+
+    public void EachInOrder(Action<T> action)
+    {
+        // TODO
+    }
+
+    public void EachPostOrder(Action<T> action)
+    {
+        // TODO
+    }
+}
---
+++ Data Structures/04. Trees and Tree-Like Structures/Exercise/4. Trees/Binary-Tree/Tree.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Tree<T>
+{
+    public Tree(T value, params Tree<T>[] children)
+    {
+      // TODO
+    }
+
+    public void Print(int indent = 0)
+    {
+        // TODO
+    }
+
+    public void Each(Action<T> action)
+    {
+        // TODO
+    }
+}
