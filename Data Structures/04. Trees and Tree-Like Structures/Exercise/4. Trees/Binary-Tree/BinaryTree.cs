edec1cd6c6753b084886ef39e6c62d5ac425542a
Dimulski <george.dimulski@gmail.com>
2016-03-06 20:41:24 +0200

    Finished Trees and Tree-Like Structures Exercise

--- Data Structures/04. Trees and Tree-Like Structures/Exercise/4. Trees/Binary-Tree/BinaryTree.cs
+++ Data Structures/04. Trees and Tree-Like Structures/Exercise/4. Trees/Binary-Tree/BinaryTree.cs
@@ -4,21 +4,59 @@ public class BinaryTree<T>
 {
     public BinaryTree(T value, BinaryTree<T> leftChild = null, BinaryTree<T> rightChild = null)
     {
-        // TODO
+        this.Value = value;
+        this.LeftChild = leftChild;
+        this.RightChild = rightChild;
     }
 
+    public T Value { get; set; }
+
+    public BinaryTree<T> LeftChild { get; set; }
+    
+    public BinaryTree<T> RightChild { get; set; }
+    
     public void PrintIndentedPreOrder(int indent = 0)
     {
-        // TODO
+        Console.Write(new string(' ', 2 * indent));
+        Console.WriteLine(this.Value);
+        if (this.LeftChild != null)
+        {
+            this.LeftChild.PrintIndentedPreOrder(indent + 1);
+        }
+
+        if (this.RightChild != null)
+        {
+            this.RightChild.PrintIndentedPreOrder(indent + 1);
+        }
     }
 
     public void EachInOrder(Action<T> action)
     {
-        // TODO
+        if (this.LeftChild != null)
+        {
+            this.LeftChild.EachInOrder(action);
+        }
+
+        action(this.Value);
+
+        if (this.RightChild != null)
+        {
+            this.RightChild.EachInOrder(action);
+        }
     }
 
     public void EachPostOrder(Action<T> action)
     {
-        // TODO
+        if (this.LeftChild != null)
+        {
+            this.LeftChild.EachPostOrder(action);
+        }
+        
+        if (this.RightChild != null)
+        {
+            this.RightChild.EachPostOrder(action);
+        }
+
+        action(this.Value);
     }
 }
