7d11efd5cc891f6af97bd344031d9e2df814ba56
Dimulski <george.dimulski@gmail.com>
2016-03-25 23:35:04 +0200

    Added Collection Data Structures and Libraries Exercise

---
+++ Data Structures/07. Collection Data Structures and Libraries/Exercise/Exercise/Part1OrderedMultiDictionary/Part1Main.cs
@@ -0,0 +1,45 @@
+﻿namespace Part1OrderedMultiDictionary
+{
+    using System;
+    using System.Globalization;
+    using System.Threading;
+
+    using Wintellect.PowerCollections;
+
+    class Part1Main
+    {
+        static void Main()
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+            var events = new OrderedMultiDictionary<DateTime, string>(true);
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string eventEntry = Console.ReadLine();
+                var eventTokens = eventEntry.Split('|');
+                string eventName = eventTokens[0].Trim();
+                DateTime eventDate = DateTime.Parse(eventTokens[1].Trim());
+                events.Add(eventDate, eventName);
+            }
+
+            var numberOfRequests = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfRequests; i++)
+            {
+                var requestLine = Console.ReadLine().Split('|');
+                DateTime startDate = DateTime.Parse(requestLine[0].Trim());
+                DateTime endDate = DateTime.Parse(requestLine[1].Trim());
+                var eventsInRange = events.Range(startDate, true, endDate, true);
+
+                Console.WriteLine(eventsInRange.KeyValuePairs.Count);
+                foreach (var e in eventsInRange)
+                {
+                    foreach (var eventName in e.Value)
+                    {
+                        Console.WriteLine("{0} | {1:dd-MMM-yyyy}", eventName, e.Key);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Data Structures/07. Collection Data Structures and Libraries/Exercise/Exercise/Part2Rope/Part2Main.cs
@@ -0,0 +1,86 @@
+﻿namespace Part2Rope
+{
+    using System;
+
+    using Wintellect.PowerCollections;
+
+    public class Part2Main
+    {
+        private static BigList<char> bigList;
+         
+        public static void Main()
+        {
+            bigList = new BigList<char>();
+
+            var inputLine = Console.ReadLine();
+            while (inputLine != null)
+            {
+                var inputLineParams = inputLine.Split(' ');
+                var command = inputLineParams[0];
+                switch (command)
+                {
+                    case "INSERT":
+                        var stringToInsert = inputLineParams[1];
+                        ExecuteInsertCommand(stringToInsert);
+                        break;
+                    case "APPEND":
+                        var stringToAppend = inputLineParams[1];
+                        ExecuteAppendCommand(stringToAppend);
+                        break;
+                    case "DELETE":
+                        var startIndex = int.Parse(inputLineParams[1]);
+                        var count = int.Parse(inputLineParams[2]);
+                        try
+                        {
+                            ExecuteDeleteCommand(startIndex, count);
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("ERROR");
+                        }
+
+                        break;
+                    case "PRINT":
+                        ExecutePrintCommand();
+                        break;
+                }
+                
+                inputLine = Console.ReadLine();
+            }
+        }
+
+        private static void ExecutePrintCommand()
+        {
+            foreach (var str in bigList)
+            {
+                Console.Write(str);
+            }
+            Console.WriteLine();
+        }
+
+        private static void ExecuteDeleteCommand(int startIndex, int count)
+        {
+            bigList.RemoveRange(startIndex, count);
+            Console.WriteLine("OK");
+        }
+
+        private static void ExecuteAppendCommand(string stringToAppend)
+        {
+            foreach (var c in stringToAppend)
+            {
+                bigList.Add(c);
+            }
+            Console.WriteLine("OK");
+        }
+
+        private static void ExecuteInsertCommand(string stringToInsert)
+        {
+            int count = 0;
+            foreach (var c in stringToInsert)
+            {
+                bigList.Insert(count++, c);
+            }
+            Console.WriteLine("OK");
+        }
+    }
+}
