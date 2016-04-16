e046bef8c558b7ac2fd1f0727fbfbc69c5d1ae4f
Dimulski <george.dimulski@gmail.com>
2016-04-16 19:41:18 +0300

    Add Sorting and Searching Algorithms Exercise

---
+++ Algorithms/April 2016/03. Sorting and Searching Algorithms/Exericse/Sortable-Collection/SortableCollection.cs
@@ -0,0 +1,81 @@
+﻿namespace Sortable_Collection
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Sortable_Collection.Contracts;
+
+    public class SortableCollection<T> where T : IComparable<T>
+    {
+        public SortableCollection()
+        {
+        }
+
+        public SortableCollection(IEnumerable<T> items)
+        {
+            this.Items = new List<T>(items);
+        }
+
+        public SortableCollection(params T[] items)
+            : this(items.AsEnumerable())
+        {
+        }
+
+        public List<T> Items { get; } = new List<T>();
+
+        public int Count => this.Items.Count;
+
+        public void Sort(ISorter<T> sorter)
+        {
+            sorter.Sort(this.Items);
+        }
+
+        public int BinarySearch(T item)
+        {
+            var result = this.BinarySearchProcedure(item, 0, this.Count - 1);
+            return result;
+        }
+
+        public int InterpolationSearch(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Shuffle()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T[] ToArray()
+        {
+            return this.Items.ToArray();
+        }
+
+        public override string ToString()
+        {
+            return $"[{string.Join(", ", this.Items)}]";
+        }
+
+        private int BinarySearchProcedure(T item, int startIndex, int endIndex)
+        {
+            if (endIndex < startIndex)
+            {
+                return -1;
+            }
+
+            int midpoint = startIndex + (endIndex - startIndex) / 2;
+            if (this.Items[midpoint].CompareTo(item) > 0)
+            {
+                return BinarySearchProcedure(item, startIndex, midpoint - 1);
+            }
+
+            if (this.Items[midpoint].CompareTo(item) < 0)
+            {
+                return BinarySearchProcedure(item, midpoint + 1, endIndex);
+            }
+
+            return midpoint;
+        }
+    }
+}
---
+++ Algorithms/April 2016/03. Sorting and Searching Algorithms/Exericse/Sortable-Collection/SortableCollectionPlayground.cs
@@ -0,0 +1,35 @@
+﻿namespace Sortable_Collection
+{
+    using System;
+
+    using Sortable_Collection.Sorters;
+
+    public class SortableCollectionPlayground
+    {
+        private static Random Random = new Random();
+
+        public static void Main()
+        {
+            const int NumberOfElementsToSort = 100;
+            const int MaxValue = 999;
+
+            var array = new int[NumberOfElementsToSort];
+
+            for (int i = 0; i < NumberOfElementsToSort; i++)
+            {
+                array[i] = Random.Next(MaxValue);
+            }
+
+            var collectionToSort = new SortableCollection<int>(array);
+            collectionToSort.Sort(new BucketSorter { Max = MaxValue });
+
+            Console.WriteLine(collectionToSort);
+
+            // var collection = new SortableCollection<int>(2, -1, 5, 0, -3);
+            // Console.WriteLine(collection);
+
+            // collection.Sort(new Quicksorter<int>());
+            // Console.WriteLine(collection);
+        }
+    }
+}
