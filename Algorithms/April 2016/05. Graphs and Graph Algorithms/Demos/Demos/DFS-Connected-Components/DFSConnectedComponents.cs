12e6bdef217c8b1688f6235f1cd46cd52854190f
Dimulski <george.dimulski@gmail.com>
2016-04-23 19:23:13 +0300

    Add 05. Graphs and Graph Algorithms

---
+++ Algorithms/April 2016/05. Graphs and Graph Algorithms/Demos/Demos/DFS-Connected-Components/DFSConnectedComponents.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class DFSConnectedComponents
+{
+    static Dictionary<string, List<string>> graph =
+        new Dictionary<string, List<string>>() {
+            { "A", new List<string>() { "D", "F" } },
+            { "B", new List<string>() { "P", "Q" } },
+            { "C", new List<string>() { "G" } },
+            { "D", new List<string>() { "A", "F" } },
+            { "E", new List<string>() { "P", "Q" } },
+            { "F", new List<string>() { "A", "D" } },
+            { "G", new List<string>() { "C" } },
+            { "H", new List<string>() { } },
+            { "I", new List<string>() { "L", "M" } },
+            { "J", new List<string>() { "K", "M", "N" } },
+            { "K", new List<string>() { "J", "N" } },
+            { "L", new List<string>() { "I", "N" } },
+            { "M", new List<string>() { "I", "J", "N" } },
+            { "N", new List<string>() { "J", "K", "L" } },
+            { "O", new List<string>() { } },
+            { "P", new List<string>() { "B", "E", "Q" } },
+            { "Q", new List<string>() { "B", "E", "P" } },
+        };
+
+    static HashSet<string> visited;
+
+    static void RecursiveDFS(string node)
+    {
+        visited.Add(node);
+        Console.Write(" " + node);
+
+        if (graph.ContainsKey(node))
+        {
+            foreach (var childNode in graph[node])
+            {
+                if (!visited.Contains(childNode))
+                {
+                    RecursiveDFS(childNode);
+                }
+            }
+        }
+    }
+
+    public static void Main()
+    {
+        visited = new HashSet<string>();
+        foreach (var node in graph.Keys)
+        {
+            if (! visited.Contains(node))
+            {
+                Console.Write("Connected component:");
+                RecursiveDFS(node);
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Algorithms/April 2016/05. Graphs and Graph Algorithms/Demos/Demos/DFS-Graph-Traversal/DFSTraverseWithStack.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class DFSTraverseWithStack
+{
+    private static Dictionary<string, List<string>> graph = 
+        new Dictionary<string, List<string>> {
+            { "Sofia", new List<string>() {
+                "Plovdiv", "Varna", "Bourgas", "Pleven", "Stara Zagora" } },
+            { "Plovdiv", new List<string>() {
+                "Bourgas", "Ruse" } },
+            { "Varna", new List<string>() {
+                "Ruse", "Stara Zagora" } },
+            { "Bourgas", new List<string>() {
+                "Plovdiv", "Pleven" } },
+            { "Ruse", new List<string>() {
+                "Varna", "Plovdiv" } },
+            { "Pleven", new List<string>() {
+                "Bourgas", "Stara Zagora" } },
+            { "Stara Zagora", new List<string>() {
+                "Varna", "Pleven" } },
+        };
+
+    private static HashSet<string> visited;
+
+    public static void Main()
+    {
+        visited = new HashSet<string>();
+        DFS("Bourgas");
+    }
+
+    public static void DFS(string node)
+    {
+        var nodes = new Stack<string>();
+
+        // Push the initial node to the stack
+        nodes.Push(node);
+        visited.Add(node);
+
+        // Iterative Depth-First Search (DFS) with stack
+        while (nodes.Count != 0)
+        {
+            string currentNode = nodes.Pop();
+            Console.WriteLine(currentNode);
+
+            var childNodes = graph[currentNode];
+            foreach (var childNode in childNodes)
+            {
+                if (!visited.Contains(childNode))
+                {
+                    nodes.Push(childNode);
+                    visited.Add(childNode);
+                }
+            }
+        }
+    }
+}
