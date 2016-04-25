4642f016de8082a8729ff8cb3899cf944770b874
Dimulski <george.dimulski@gmail.com>
2016-04-25 23:25:48 +0300

    Finish Sorting and Searching Algorithms Homework

--- Algorithms/April 2016/03. Sorting and Searching Algorithms/Homework/Sortable-Collection.Tests/SortTests.cs
+++ Algorithms/April 2016/03. Sorting and Searching Algorithms/Homework/Sortable-Collection.Tests/SortTests.cs
@@ -11,7 +11,7 @@
     [TestClass]
     public class SortTests
     {
-        private static readonly ISorter<int> TestSorter = new MergeSorter();
+        private static readonly ISorter<int> TestSorter = new InPlaceMergeSorter<int>();
 
         private static readonly Random Random = new Random();
 
--- Algorithms/April 2016/03. Sorting and Searching Algorithms/Homework/Sortable-Collection/Sorters/InPlaceMergeSorter.cs
+++ Algorithms/April 2016/03. Sorting and Searching Algorithms/Homework/Sortable-Collection/Sorters/InPlaceMergeSorter.cs
@@ -9,7 +9,41 @@
     {
         public void Sort(List<T> collection)
         {
-            throw new NotImplementedException();
+            this.InPlaceMergeSort(collection, 0, collection.Count - 1);
+        }
+
+        private void InPlaceMergeSort(List<T> collection, int start, int end)
+        {
+            if (start < end)
+            {
+                int mid = (end + start) / 2;
+                this.InPlaceMergeSort(collection, start, mid);
+                this.InPlaceMergeSort(collection, mid + 1, end);
+
+                int left = start;
+                int right = mid + 1;
+                if (collection[mid].CompareTo(collection[right]) <= 0)
+                {
+                    return;
+                }
+
+                while (left <= mid && right <= end)
+                {
+                    if (collection[left].CompareTo(collection[right]) <= 0)
+                    {
+                        left++;
+                    }
+                    else
+                    {
+                        T temp = collection[right];
+                        collection.RemoveAt(right);
+                        collection.Insert(left, temp);
+                        left++;
+                        right++;
+                        mid++;
+                    }
+                }
+            }
         }
     }
 }
