d0af2b07556799a18b3cc0c55990355cd3d737d4
Dimulski <george.dimulski@gmail.com>
2016-04-29 21:44:39 +0300

    Work on Dynamic Programming Exercise

--- Algorithms/April 2016/04. Dynamic Programming/Exercise/Knapsack-Problem/Knapsack.cs
+++ Algorithms/April 2016/04. Dynamic Programming/Exercise/Knapsack-Problem/Knapsack.cs
@@ -1,4 +1,4 @@
-﻿namespace Knapsack_Problem
+﻿namespace KnapsackProblem
 {
     using System;
     using System.Collections.Generic;
@@ -27,10 +27,7 @@
             Console.WriteLine("Take the following items in the knapsack:");
             foreach (var item in itemsTaken)
             {
-                Console.WriteLine(
-                    "  (weight: {0}, price: {1})",
-                    item.Weight,
-                    item.Price);
+                Console.WriteLine("(weight: {0}, price: {1})", item.Weight, item.Price);
             }
 
             Console.WriteLine("Total weight: {0}", itemsTaken.Sum(i => i.Weight));
@@ -39,8 +36,51 @@
 
         public static Item[] FillKnapsack(Item[] items, int capacity)
         {
-            // TODO
-            throw new NotImplementedException();
+            var maxPrice = new int[items.Length, capacity + 1];
+            var isItemTaken = new bool[items.Length, capacity + 1];
+
+            for (int indexCapacity = 0; indexCapacity <= capacity; indexCapacity++)
+            {
+                if (items[0].Weight <= indexCapacity)
+                {
+                    maxPrice[0, indexCapacity] = items[0].Price;
+                    isItemTaken[0, indexCapacity] = true;
+                }
+            }
+
+            for (int indexItem = 1; indexItem < items.Length; indexItem++)
+            {
+                for (int indexCapacity = 0; indexCapacity <= capacity; indexCapacity++)
+                {
+                    maxPrice[indexItem, indexCapacity] = maxPrice[indexItem - 1, indexCapacity];
+
+                    var remainingCapacity = indexCapacity - items[indexItem].Weight;
+                    if (remainingCapacity >= 0 && maxPrice[indexItem - 1, remainingCapacity] + items[indexItem].Price > maxPrice[indexItem - 1, indexCapacity])
+                    {
+                        maxPrice[indexItem, indexCapacity] = maxPrice[indexItem - 1, remainingCapacity] + items[indexItem].Price;
+                        isItemTaken[indexItem, indexCapacity] = true;
+                    }
+                }
+            }
+
+            return TakeItems(items, isItemTaken, capacity);
+        }
+
+        private static Item[] TakeItems(Item[] items, bool[,] isItemTaken, int capacity)
+        {
+            var itemsTaken = new List<Item>();
+
+            for (int itemIndex = items.Length - 1; itemIndex >= 0; itemIndex--)
+            {
+                if (isItemTaken[itemIndex, capacity])
+                {
+                    itemsTaken.Add(items[itemIndex]);
+                    capacity -= items[itemIndex].Weight;
+                }
+            }
+
+            itemsTaken.Reverse();
+            return itemsTaken.ToArray();
         }
     }
 }
