d6ea640ed68a93e2a5a43d8bac0de969e7ceb97f
Dimulski <george.dimulski@gmail.com>
2016-06-02 23:57:57 +0300

    Fix Balanced Parentheses

--- Advanced Java/01. Stacks and Queues/Exercise/src/Part3ExercisesWithStacksAndQueues/Problem7BalancedParentheses/BalancedParentheses.java
+++ Advanced Java/01. Stacks and Queues/Exercise/src/Part3ExercisesWithStacksAndQueues/Problem7BalancedParentheses/BalancedParentheses.java
@@ -9,57 +9,85 @@ import java.util.Stack;
 public class BalancedParentheses {
 
     static String input;
-    static Stack<Integer> normalBrackets;
-    static Stack<Integer> curlyBrackets;
-    static Stack<Integer> squareBrackets;
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
         input = scanner.nextLine();
 
-        normalBrackets = new Stack<>();
-        curlyBrackets = new Stack<>();
-        squareBrackets = new Stack<>();
-
         boolean bool = ValidateSequence(0, input.length());
-        PrintOutputAndExit(bool);
+        PrintOutput(bool);
     }
 
     private static boolean ValidateSequence(int start, int end) {
 
-        int length = Math.abs(start - end);
+        Stack<Integer> normalBrackets = new Stack<>();
+        Stack<Integer> curlyBrackets = new Stack<>();
+        Stack<Integer> squareBrackets = new Stack<>();
+        int i2 = 0;
 
         for (int i = start; i < end; i++) {
-            if (input.charAt(i) == '(') {
-                normalBrackets.add(i);
-            }
-            if (input.charAt(i) == '{') {
-                curlyBrackets.add(i);
-            }
-            if (input.charAt(i) == '[') {
-                squareBrackets.add(i);
-            }
-            if (input.charAt(i) == ')') {
-                if (normalBrackets.empty() || Math.abs(normalBrackets.peek() - i) % 2 != 0) {
-                    PrintOutputAndExit(false);
-                } else {
-                    boolean bool = ValidateSequence(normalBrackets.peek() + 1, i);
-                    if (bool) {
-                        input = input.substring(0,start) + input.substring(end, input.length());
+            i2 = i;
+            if (input.charAt(i) == 'x') {
+            } else {
+                if (input.charAt(i) == '(') {
+                    normalBrackets.add(i);
+                }
+                else if (input.charAt(i) == '{') {
+                    curlyBrackets.add(i);
+                }
+                else if (input.charAt(i) == '[') {
+                    squareBrackets.add(i);
+                }
+                else if (input.charAt(i) == ')') {
+                    if (normalBrackets.empty() || (normalBrackets.peek() - i - 1) % 2 != 0) {
+                        return false;
+                    } else {
+                        ValidateSequence(normalBrackets.pop() + 1, i - 1);
+                    }
+                }
+                else if (input.charAt(i) == '}') {
+                    if (curlyBrackets.empty() || (curlyBrackets.peek() - i - 1) % 2 != 0) {
+                        return false;
+                    } else {
+                        ValidateSequence(curlyBrackets.pop() + 1, i - 1);
+                    }
+                }
+                else if (input.charAt(i) == ']') {
+                    if (squareBrackets.empty() || (squareBrackets.peek() - i - 1) % 2 != 0) {
+                        return false;
+                    } else {
+                        ValidateSequence(squareBrackets.pop() + 1, i - 1);
                     }
                 }
             }
         }
 
+        if (!normalBrackets.empty() || !curlyBrackets.empty() || !squareBrackets.empty()) {
+            return false;
+        } else {
+            if (i2 < input.length() - 1){
+                int lengthOfCheckedString = 0;
+                for (int i = start - 1; i <= end + 1; i++) {
+                    lengthOfCheckedString++;
+                }
+                StringBuilder fillerString = new StringBuilder();
+
+                for (int j = 0; j < lengthOfCheckedString; j++) {
+                    fillerString.append("x");
+                }
+                input = input.substring(0,start - 1) + fillerString.toString() + input.substring(end + 2);
+            }
+
+        }
+        return true;
     }
 
-    private static void PrintOutputAndExit(boolean bool) {
+    private static void PrintOutput(boolean bool) {
         if (bool) {
             System.out.println("YES");
         } else {
             System.out.println("NO");
         }
-        System.exit(0);
     }
 }
