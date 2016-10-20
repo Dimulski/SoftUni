90d46c716de35512a6d12c8b5064d825b66e40e4
George <george.dimulski@gmail.com>
2016-10-20 15:06:26 +0300

    Revert "Refactor file structure"
    This reverts commit dedb55c97ff3f7d60a77bcf6fa3a5dc80c7bfc75.

---
+++ Advanced C#/09. Exam Preparation/Exam 19 July 2015/Exam 19 July 2015/Problem2ArraySlider/ArraySliderMain.cs
@@ -0,0 +1,76 @@
+﻿namespace Problem2ArraySlider
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Numerics;
+
+    class ArraySliderMain
+    {
+        static void Main()
+        {
+            //List<BigInteger> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            BigInteger[] numbers = Console.ReadLine()
+                .Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(BigInteger.Parse)
+                .ToArray();
+
+            int listLength = numbers.Length;
+            int currentIndex = 0;
+            string command = Console.ReadLine();
+            while (command != "stop")
+            {
+                string[] commandParams = command.Split();
+                int offset = int.Parse(commandParams[0]) % listLength;
+                char operation = Char.Parse(commandParams[1]);
+                int operand = int.Parse(commandParams[2]);
+
+                if (offset < 0)
+                {
+                    offset += listLength;
+                }
+
+                currentIndex = (currentIndex + offset) % listLength;
+
+                DoDatOperation(numbers, currentIndex, operation, operand);
+
+                command = Console.ReadLine();
+            }
+            Console.WriteLine("[{0}]", string.Join(", ", numbers));
+        }
+
+
+        private static void DoDatOperation(BigInteger[] numbers, int offset, char operation, int operand)
+        {
+            switch (operation)
+            {
+                case '&':
+                    numbers[offset] &= operand;
+                    break;
+                case '|':
+                    numbers[offset] |= operand;
+                    break;
+                case '^':
+                    numbers[offset] ^= operand;
+                    break;
+                case '+':
+                    numbers[offset] += operand;
+                    break;
+                case '-':
+                    numbers[offset] -= operand;
+                    break;
+                case '*':
+                    numbers[offset] *= operand;
+                    break;
+                case '/':
+                    numbers[offset] /= operand;
+                    break;
+            }
+
+            if (numbers[offset] < 0)
+            {
+                numbers[offset] = 0;
+            }
+        }
+    }
+}
