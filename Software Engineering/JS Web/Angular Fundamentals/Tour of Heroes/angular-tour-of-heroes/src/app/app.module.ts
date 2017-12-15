0858210ab09bab60e09b6d5ad5c95e718c134ab8
George <george.dimulski@gmail.com>
2017-12-15 23:52:34 +0200

    Work on Tour of Heroes

--- Software Engineering/JS Web/Angular Fundamentals/Tour of Heroes/angular-tour-of-heroes/src/app/app.module.ts
+++ Software Engineering/JS Web/Angular Fundamentals/Tour of Heroes/angular-tour-of-heroes/src/app/app.module.ts
@@ -5,18 +5,24 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroService } from './hero.service';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageService } from './message.service';
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
+    MessagesComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [ HeroService, MessageService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
